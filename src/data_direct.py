b846871958d86a63e39fd2bf508e0fcb7ac1f349
Alexander Ament <alexcament@gmail.com>
2025-06-27 08:38:19 -0400

    Revert "Add automatic update checking"
    This reverts commit dcc883a3b68c8468e5481cd05926ed6ff43d5da6.

--- src/data_direct.py
+++ src/data_direct.py
@@ -3,13 +3,11 @@ __version__ = "1.2.0"
 import traceback
 import misc.ui as ui
 from venues import venue_report
-from misc.updateChecks import check_for_update
 
 ui.clear(__version__)
 print('[Begin Program]')
 
 try:
-    check_for_update(__version__)
     venue_report.generate()
 except Exception as e:
     ui.print_error(f'An unexpected error occurred while generating the venue report: {e}')
