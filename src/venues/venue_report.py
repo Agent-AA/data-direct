1a8360a480c06629c05166e5ef90c442894bf819
Alexander Ament <alexcament@gmail.com>
2025-07-08 21:04:53 -0400

    Remove test indications

--- src/venues/venue_report.py
+++ src/venues/venue_report.py
@@ -37,7 +37,7 @@ def generate(venue_records: set['VenueRecord']=None):
         ui.pause()
 
         # Prompt for excel file
-        file_path = _get_file_path(test=True)
+        file_path = _get_file_path(test=False)
 
         # Load file
         headers, raw_data_sheet = _load_excel(file_path)
@@ -120,8 +120,8 @@ def generate(venue_records: set['VenueRecord']=None):
     ui.prompt_user('\nThis program will now prompt you to select an ouput directory. Press any key to continue.')
     ui.pause()
     
-    #selected_dir = ui.promptDirectory()
-    selected_dir = 'C:\\Users\\alexc\\Documents\\data-direct\\test'
+    selected_dir = ui.promptDirectory()
+    #selected_dir = 'C:\\Users\\alexc\\Documents\\data-direct\\test'
 
     if selected_dir == '':
         ui.print_error('No directory selected. Terminating report.')
